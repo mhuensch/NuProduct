744321c7977aaf8ebdff9308d254094594540f37
Michael Huensch <setup@run00.com>
2014-03-23 15:41:38 -0500

    Updating package installation to include path to the tools folder for the exe

---
+++ Run00.NuProductWindowsConsole/Install.ps1
@@ -0,0 +1,3 @@
+﻿param($installPath, $toolsPath, $package, $project)
+$text = (gc $toolsPath\Run00.NuProductWindowsConsoleAdditional.props.template).replace("[path]","$($toolsPath)\Run00.NuProductWindowsConsole.exe")
+$text | Set-Content "$($installPath)\build\Run00.NuProductWindowsConsoleAdditional.props"
